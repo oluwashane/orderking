adf3dbce5a333473be44bd2656f6af0b5cd021a2
oluwashane <ogbulaforudochukwu@gmail.com>
2020-05-11 16:34:21 +0100

    fixed error on the cart and disabled badge for now

--- assets/script/order.js
+++ assets/script/order.js
@@ -1,69 +1,13 @@
 // variables
 const orderSection = document.querySelector(".order");
 const shoppingCart = document.querySelector("#shoppingCart");
-const orderCloseButton = document.querySelector("#orderCloseButton");
+const cartCloseButton = document.querySelector("#orderCloseButton");
 const orderContent = document.querySelector(".orderContent");
 const badge = document.querySelector(".badge");
 let cartBody = document.querySelector("#tbody");
 
+
 let badgeCount;
-
-window.addEventListener('load', function(e) {
-    console.log('All assets are loaded')
-    const checkStorage = JSON.parse(localStorage.getItem("orderArr"));
-    if (!checkStorage) {
-        this.console.log("No storage allocated")
-    } else {
-        badgeCount = checkStorage.length;
-        increaseBadge()
-        checkStorage.forEach((data) => {
-            cartBody.innerHTML += `
-                <tr>
-                    <th scope="row"></th>
-                    <td>${data.name}</td>
-                    <td>${data.price}</td>
-                    <td><input type="button" value="x"></td>
-                </tr>`
-        })
-    }
-    e.preventDefault()
-})
-
-// Badge section
-function increaseBadge() {
-    badge.innerHTML = badgeCount++
-}
-
-// menu section
-const orderBtn = document.querySelector(".menu");
-orderBtn.addEventListener("click", addOrder)
-
-function addOrder (e) {
-    const menuItemName = e.target.previousElementSibling.children[1].innerText;
-    const menuItemPrice = e.target.previousElementSibling.children[3].innerText;
-    if (e.target.classList.value === "orderBtn") {
-        storeInLocalStorage(menuItemName, menuItemPrice); 
-        cartBody.innerHTML += `
-                <tr>
-                    <th scope="row"></th>
-                    <td>${menuItemName}</td>
-                    <td>${menuItemPrice}</td>
-                    <td><input type="button" value="x"></td>
-                </tr>` 
-        increaseBadge();
-    }
-    e.preventDefault()
-}
-
-function storeInLocalStorage (name, price) {
-    const oldItems = JSON.parse(localStorage.getItem("orderArr")) || [];
-    const order = { 
-        name,
-        price
-    }
-    oldItems.push(order);
-    localStorage.setItem("orderArr", JSON.stringify(oldItems));
-}
 
 
 // Shopping cart icon
@@ -73,10 +17,135 @@ shoppingCart.addEventListener("click", (e) => {
     e.preventDefault()
 })
 
-orderCloseButton.addEventListener("click", (e) => {
+cartCloseButton.addEventListener("click", (e) => {
     console.log("closed!")
     orderSection.style.display = "none";
     e.preventDefault();
 })
+
+// window.addEventListener('load', function(e) {
+//     const checkStorage = JSON.parse(localStorage.getItem("orderArr"));
+//     if (!checkStorage) {
+//         // this.console.log("No storage allocated")
+//     } else {
+//         badgeCount = checkStorage.length;
+//         increaseBadge()
+//         checkStorage.forEach((data) => {
+//             cartBody.innerHTML += `
+//                 <tr>
+//                     <th scope="row"></th>
+//                     <td>${data.name}</td>
+//                     <td>${data.price}</td>
+//                     <td><input type="button" value="x"></td>
+//                 </tr>`
+//         })
+//     }
+//     e.preventDefault()
+// })
+
+// Badge section
+
+function increaseBadge() {
+    badge.innerHTML = badgeCount++
+}
+
+const orderButton = document.querySelectorAll('.orderBtn');
+for (let i = 0; i < orderButton.length; i++) {
+    const button = orderButton[i];
+    button.addEventListener('click', addClickedItem)
+}
+
+function addClickedItem (e) {
+    const button = e.target;
+    const menuItem = button.previousElementSibling;
+    const title = menuItem.getElementsByClassName("menuItemName")[0].innerText;
+    const price = menuItem.getElementsByClassName("menuItemPrice")[0].innerText;
+    addItemToCart(title, price);
+    e.preventDefault()
+}
+
+
+function addItemToCart (itemName, itemPrice) {
+    // const defaultQty = 1;
+    const cartRow = document.createElement('tr');
+    cartRow.className ='cartRow';
+    const cartContent = `
+                <th scope="row">
+                    <input type="text" name="quantity" class="cartQuantityInput" value = 1>
+                </th>
+                <td>${itemName}</td>
+                <td class="cartPrice">${itemPrice}</td>
+                <td><input type="button" value="x" class="removeOrder"></td>
+        `
+    cartRow.innerHTML = cartContent;
+    cartBody.append(cartRow);
+    updateCartTotal()
+    updateCart()
+}
+
+
+function updateCart() {
+    const removeCartItemButtons = document.getElementsByClassName("removeOrder");
+    for(let i = 0; i < removeCartItemButtons.length; i++) {
+        const button = removeCartItemButtons[i];
+        button.addEventListener('click', (e) => {
+            const buttonClicked = e.target
+            buttonClicked.parentElement.parentElement.remove();
+            updateCartTotal();
+            console.log('Remove Button clicked')
+        })
+    }
+
+    const quantityInputs = document.getElementsByClassName("cartQuantityInput");
+    for(let i = 0; i < quantityInputs.length; i++) {
+        const input = quantityInputs[i];
+        input.addEventListener('change', quantityChanged)
+    }
+}
+
+
+
+
+// function storeInLocalStorage (name, price) {
+//     const oldItems = JSON.parse(localStorage.getItem("orderArr")) || [];
+//     const order = { 
+//         name,
+//         price
+//     }
+//     oldItems.push(order);
+//     localStorage.setItem("orderArr", JSON.stringify(oldItems));
+// }
+
+
+
+function quantityChanged(e) {
+    const input = e.target;
+    // Check if input is a number or not
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1;
+    }
+    updateCartTotal()
+}
+
+
+
+function updateCartTotal() {
+    const cartItemContainer = document.getElementsByClassName("cartContainer")[0];
+    const cartRows = cartItemContainer.getElementsByClassName("cartRow");
+    let total = 0;
+    for (let i = 0; i < cartRows.length; i++) {
+        const cartRow = cartRows[i];
+        const priceElement = cartRow.getElementsByClassName("cartPrice")[0];
+        const quantityElement = cartRow.getElementsByClassName("cartQuantityInput")[0];
+        const price = parseFloat(priceElement.innerText.replace('₦', ''))
+        const quantity = quantityElement.value;
+        total = total + (price * quantity);
+    }
+    //Round total to two decimal place
+    total = Math.round(total * 100)/100
+    document.getElementsByClassName("cartTotalPrice")[0].innerText = `₦${total}`;
+}
+
+
 
 
